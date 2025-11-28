bf2ad2c6ad9b4dc83d476ee4fda7aca85122bc5a
Junaid-khan-01-web <jk620020@gmail.com>
2025-11-28 12:25:37 +0530

    Contract updated

--- contracts/EtherLendDAO.sol
+++ contracts/EtherLendDAO.sol
@@ -1,62 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
-
-/**
- * @title EtherLend DAO
- * @notice Governance contract allowing token holders to create, vote and execute proposals.
- * @dev This is a template. Add safety checks, timelock and lending integrations as needed.
- */
-
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address to, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
-    function mint(address to, uint256 amount) external;
-}
-
-contract EtherLendToken {
-    string public name = "EtherLend Governance Token";
-    string public symbol = "ELT";
-    uint8 public decimals = 18;
-
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    address public dao;
-
-    modifier onlyDAO() {
-        require(msg.sender == dao, "Not DAO");
-        _;
-    }
-
-    constructor(address _dao) {
-        dao = _dao;
-    }
-
-    function mint(address to, uint256 amount) external onlyDAO {
-        totalSupply += amount;
-        balanceOf[to] += amount;
-    }
-
-    function transfer(address to, uint256 amount) external returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Bal < amount");
-        balanceOf[msg.sender] -= amount;
-        balanceOf[to] += amount;
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-        require(balanceOf[from] >= amount, "Bal < amount");
-        balanceOf[from] -= amount;
-        balanceOf[to] += amount;
-        return true;
-    }
-}
-
-contract EtherLendDAO {
-    // ----------------------------------------------------------
-    // STRUCTS
-    // ----------------------------------------------------------
+----------------------------------------------------------
+    ----------------------------------------------------------
     struct Proposal {
         uint256 id;
         address proposer;
@@ -70,11 +13,8 @@ contract EtherLendDAO {
         bytes callData;
     }
 
-    // ----------------------------------------------------------
-    // STATE VARIABLES
-    // ----------------------------------------------------------
-    EtherLendToken public token;
-    uint256 public constant VOTING_DURATION = 20000;  // ~3 days on Ethereum
+    STATE VARIABLES
+    ~3 days on Ethereum
     uint256 public proposalCount;
     mapping(uint256 => Proposal) public proposals;
     mapping(uint256 => mapping(address => bool)) public voted;
@@ -82,45 +22,20 @@ contract EtherLendDAO {
     address public treasury;
     address public owner;
 
-    // ----------------------------------------------------------
-    // EVENTS
-    // ----------------------------------------------------------
-    event ProposalCreated(
-        uint256 indexed id,
-        address indexed proposer,
-        string description,
-        address target,
-        uint256 value
-    );
-    event Vote(uint256 indexed id, address indexed voter, bool support, uint256 weight);
-    event Executed(uint256 indexed id);
-
-    // ----------------------------------------------------------
-    // MODIFIERS
-    // ----------------------------------------------------------
+    EVENTS
+    ----------------------------------------------------------
+    ----------------------------------------------------------
     modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
         _;
     }
 
-    // ----------------------------------------------------------
-    // CONSTRUCTOR
-    // ----------------------------------------------------------
-    constructor() {
-        owner = msg.sender;
-
-        // Create governance token and give DAO minting rights
+    CONSTRUCTOR
+    Create governance token and give DAO minting rights
         token = new EtherLendToken(address(this));
 
-        // Mint initial tokens to deployer (bootstrap)
-        token.mint(msg.sender, 1_000_000 ether);
-
-        treasury = address(this);
-    }
-
-    // ----------------------------------------------------------
-    // DAO CORE
-    // ----------------------------------------------------------
+        ----------------------------------------------------------
+    ----------------------------------------------------------
     function createProposal(
         string calldata description,
         address target,
@@ -178,8 +93,7 @@ contract EtherLendDAO {
         emit Executed(id);
     }
 
-    // ----------------------------------------------------------
-    // TREASURY FUNCTIONS
+    TREASURY FUNCTIONS
     // ----------------------------------------------------------
     receive() external payable {}
 
@@ -188,3 +102,6 @@ contract EtherLendDAO {
         payable(to).transfer(amount);
     }
 }
+// 
+Contract End
+// 
